6d34b8c333eb47b0e19b8bcbc97605b7d439a0ae
Filip Johnsson <filip.johnsson1989@gmail.com>
2021-11-30 16:16:26 +0100

    Add Emploee class

---
+++ Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personalregister
+{
+    class Employee
+    {
+        private string _name;
+        private int _wage;
+
+        public string Name { get { return _name; } }
+        public int Wage { get { return _wage; } }
+
+        public Employee(string name,int wage)
+        {
+            _name = name;
+            _wage = wage;
+        }
+    }
+}
