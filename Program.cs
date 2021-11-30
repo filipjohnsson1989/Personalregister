ed25f398438ea1c466e5e7c1467718f1fe5736b6
Filip Johnsson <filip.johnsson1989@gmail.com>
2021-11-30 16:18:18 +0100

    Add menu options

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Personalregister
 {
@@ -6,6 +7,40 @@ namespace Personalregister
     {
         static void Main(string[] args)
         {
+            List<Employee> employees = new List<Employee>();
+
+            bool stopProgramm = false;
+            int inputNumber;
+
+            while (!stopProgramm)
+            {
+                Console.WriteLine("Vad vill du göra?");
+                Console.WriteLine("1- Ta emot och lagra anställda.");
+                Console.WriteLine("2- Skriva ut registret.");
+                Console.WriteLine("3- Avsluta.");
+
+                var stringInput = Console.ReadLine();
+                if (int.TryParse(stringInput, out inputNumber))
+                {
+                    switch (inputNumber)
+                    {
+                        case 1:
+
+                            break;
+
+                        case 2:
+                            break;
+
+                        case 3:
+                            stopProgramm = true;
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
         }
+
     }
 }
